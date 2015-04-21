aed5e0ffd0ba53690bd377f40cbc2dd31571204a
Laith <info@laithazzam.com>
2015-04-20 22:19:03 -0700

    bwm

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -13,9 +13,10 @@
 		emails: $('.wrapper>div'),
 		paddingTop:window.innerHeight,
 		messages:'',
+		dayCount: '',
 		init: function(){
 			site.spreadsheet();
-			site.miscFunctions(); 
+			site.miscFunctions();   
 		},
 		probe: function(){
 			$('#logo').attr('src','assets/img/logo.svg');
@@ -48,6 +49,7 @@
 		resize: function(){
 		},
 		paddingBottom: function(){
+			$('.message').css('padding-bottom','0px');
 			paddingBottom = ( site.paddingTop-$('.wrapper>div').last().height() )/2;
 			if(paddingBottom <= 50){
 				paddingBottom = 100
@@ -64,7 +66,7 @@
 		},
 
 		userTime: function(){
-			var newDate = moment().year(2010).month(5);
+			var newDate = moment().year(2010);
 			return newDate.unix();
 		},
 
@@ -78,122 +80,145 @@
 
 			displayNext();
 
-			$('#backToTop').on('click',function(){
-				displayNext();
-			});
-
 			$("#facebook").click(function() {
-				FB.ui({
-				  method: 'share',
-				  href: 'http://beforewemet.com/',
-				}, function(response){});
+				if(navigator.userAgent.match('CriOS')){
+					console.log('ios Chrome');
+				}else {
+					FB.ui({
+					  method: 'share',
+					  href: 'http://beforewemet.com/',
+					}, function(response){});
+					return false;
+				}
 			});
 
 			function displayNext() {
+			console.log($('.message').length, site.messages.length);
+			if($('.message').length < site.messages.length){
 				Waypoint.destroyAll()
-			  $('.message').last().css('padding-bottom','0px');
-			  // get the list element
-			  var amount = 0;
-			  var list = $('.wrapper');
-			  
-			  // get index stored as a data on the list, if it doesn't exist then assign 0
-			  var index = list.data('index') % messages.length || 0;
-			  
-			  // 1) get 20 elements from array - starting from index, using Array.slice()
-			  // 2) map them to array of li strings
-			  // 3) join the array into a single string and set it as a HTML content of list
-				
-			  $.each(messages.slice(index, index + 10), function(index, val) {
+				// get the list element
+				var amount = 0;
+				var list = $('.wrapper');
 
-				var item_date = moment(""+val.DATE+" "+val.TIME+"");
-				item_miliseconds = item_date.unix();
+				// get index stored as a data on the list, if it doesn't exist then assign 0
+				var index = list.data('index') % messages.length || 0;
 
-				if(current_miliseconds >= item_miliseconds ){
-					amount++;
-					parseMessage();
-					last = $('.message').last();
-					// if(last.hasClass(''+side+'') && type == 'gchat'){
-					// 	$('.message').last().append('<p>'+val.COPY+'</p>');
-					// }
-					if(author != 'none'){
-			    		$('.wrapper').append('<div class="'+side+' '+type+' message"><h2>'+subject+'</h2><p>'+copy+'</p></div>');
-			    		$('.message').last().children('p').linkify();
-					}
-					if(item_attachments && val.SUBJECT != "gchat" && author != 'none'){
-						item = wrapper.lastChild.getElementsByTagName("p")[0];
-						item.appendChild(item_attachments);
-					}
-				}
-				list.data('index', index + amount);
-				function parseMessage(){
-					author();
-					subject();
-					copy();
-					attachments();
-				};
-			  	function author(){
-					// set author per item
-					if(val.AUTHOR.substring(0,3) == 'tal'){
-						author = val.AUTHOR.substring(0,3);
-						side = 'right';
-					}else if(val.AUTHOR.substring(0,4) == 'josh'){
-						author = val.AUTHOR.substring(0,4);
-						side = 'left'
-					}else{
-						author = 'none';
-						side = 'middle someone'
-					}
-				};
-				function subject(){
-					// use subject to determine if message is ghat or email
-					if(val.SUBJECT == "gchat"){
-						// if gchat, set subject to nothing to know it's gchat
-						subject = '';
-						type = 'gchat';
-					}else{
-						subject = ''+val.SUBJECT+'';
-						type = '';
-					}
-				};
-				function attachments(){
-					if(val.ATTACHMENTS == ''){
-						item_attachments = false;
-					}else{
-						item_attachments_raw = JSON.stringify(val.ATTACHMENTS).split(/[\\]n/g), parts = [];
+				// 1) get 20 elements from array - starting from index, using Array.slice()
+				// 2) map them to array of li strings
+				// 3) join the array into a single string and set it as a HTML content of list
 
-						function makeUL(array) {
-						    // Create the list element:
-						    var list = document.createElement('ul');
+				$.each(messages.slice(index, index + messages.length), function(index, val) {
 
-						    for(var i = 0; i < array.length; i++) {
-						        // Create the list item:
-						        var link = document.createElement('a');
-						        link.href = 'assets/attachments/'+array[i].replace(/\"/g, "");
-						        link.target = '_blank';
-						        var item = document.createElement('li');
+					var item_date = moment(""+val.DATE+" "+val.TIME+"");
+					item_miliseconds = item_date.unix();
 
-						        // Set its contents:
-						        item.appendChild(link).appendChild(document.createTextNode(array[i].replace(/\"/g, "")));
-
-						        // Add it to the list:
-						        list.appendChild(item);
-						    }
-						    // Finally, return the constructed list:
-						    return list;
+					if(current_miliseconds >= item_miliseconds ){
+						amount++;
+						parseMessage();
+						last = $('.message').last();
+						lastDate = messages[last.index()];
+						if(author != 'none'){
+							$('.wrapper').append('<div class="'+side+' '+type+' message"><h2>'+subject+'</h2><p>'+copy+'</p></div>');
+							$('.message').last().children('p').linkify();
+							
+							if(last.index()>0){
+								if(lastDate.DATE != val.DATE){
+								 	site.dayCount++;
+									newLast = $('div.message').last();
+									newLast.prepend('<span><img src="assets/img/days/day'+(site.dayCount+1)+'.svg" alt="day1" /></span>');
+								}
+							}
 						}
-						item_attachments = makeUL(item_attachments_raw);
+						if(item_attachments && val.SUBJECT != "gchat" && author != 'none'){
+							item = wrapper.lastChild.getElementsByTagName("p")[0];
+							item.appendChild(item_attachments);
+						}
+
 					}
-				}
-				function copy(){
-					copyRaw = JSON.stringify(val.COPY).replace(/[\\]n/g, '<br/>').replace(/\\/g, "");
-					copy = copyRaw.substring(1, copyRaw.length-1);
-				};
+					function parseMessage(){
+						author();
+						subject();
+						copy();
+						attachments();
+					};
+						function author(){
+						// set author per item
+						if(val.AUTHOR.substring(0,3) == 'tal'){
+							author = val.AUTHOR.substring(0,3);
+							side = 'right';
+						}else if(val.AUTHOR.substring(0,4) == 'josh'){
+							author = val.AUTHOR.substring(0,4);
+							side = 'left'
+						}else{
+							author = 'none';
+							side = 'middle someone'
+						}
+					};
+					function subject(){
+						// use subject to determine if message is ghat or email
+						if(val.SUBJECT == "gchat"){
+							// if gchat, set subject to nothing to know it's gchat
+							subject = '';
+							type = 'gchat';
+						}else{
+							subject = ''+val.SUBJECT+'';
+							type = '';
+						}
+					};
+					function attachments(){
+						if(val.ATTACHMENTS == ''){
+							item_attachments = false;
+						}else{
+							item_attachments_raw = JSON.stringify(val.ATTACHMENTS).split(/[\\]n/g), parts = [];
+
+							function makeUL(array) {
+							    // Create the list element:
+							    var list = document.createElement('ul');
+
+							    for(var i = 0; i < array.length; i++) {
+							        // Create the list item:
+							        var link = document.createElement('a');
+							        link.href = 'assets/attachments/'+array[i].replace(/\"/g, "");
+							        link.target = '_blank';
+							        var item = document.createElement('li');
+
+							        // Set its contents:
+							        item.appendChild(link).appendChild(document.createTextNode(array[i].replace(/\"/g, "")));
+
+							        // Add it to the list:
+							        list.appendChild(item);
+							    }
+							    // Finally, return the constructed list:
+							    return list;
+							}
+							item_attachments = makeUL(item_attachments_raw);
+						}
+					}
+					function copy(){
+						copyRaw = JSON.stringify(val.COPY).replace(/[\\]n/g, '<br/>').replace(/\\/g, "");
+						copy = copyRaw.substring(1, copyRaw.length-1);
+					};
+
+				});
+				list.data('index', parseInt(index) + parseInt(amount));
+				if(amount > 0 ){
+
+					$('#header').css('height','75%');
+					var el = document.getElementById('header');
+					el.addEventListener("transitionend", function(){
+						site.postBuild();
+					}, true);	
 				
-			});
-				if($(".wrapper").children('div').length){
-					site.postBuild();
+					// // site.postBuild();
+					// $('.wrapper').waypoint(function() {
+					//  	displayNext();
+					// }, {
+					//  triggerOnce: true,
+					//  offset: 'bottom-in-view'
+					// });
 				}
 			}
+		}
 		},
 		postBuild: function(){
 
@@ -242,26 +267,21 @@
 			    })
 			  })
 			});
-				// $('#header').css('height','75%');
-				// var el = document.getElementById('header');
-				// el.addEventListener("transitionend", function(){
-				// 	// $(el).removeClass('animate');
-				// }, true);
 		},
 		miscFunctions: function(){
-			// $("#backToTop").click(function() {
-			// 	$('html, body').stop();
-			// 	$('html, body').css('overflow','hidden');
-			// 	var scrollTop = $(document).scrollTop();
-			// 	speed = (scrollTop-$(document).height())/2;
-			// 	if(speed < 3000 || speed > 3000){
-			// 		speed = 3000;
-			// 	}
-			// 	$("html, body").animate({ scrollTop: $(document).height() }, speed, function(){
-			// 		$('html, body').css('overflow','auto');
-			// 	});
-			//   return false;
-			// });
+			$("#backToTop").click(function() {
+				$('html, body').stop();
+				$('html, body').css('overflow','hidden');
+				var scrollTop = $(document).scrollTop();
+				speed = (scrollTop-$(document).height())/2;
+				if(speed < 3000 || speed > 3000){
+					speed = 3000;
+				}
+				$("html, body").animate({ scrollTop: $(document).height() }, speed, function(){
+					$('html, body').css('overflow','auto');
+				});
+			  return false;
+			});
 			$("#aboutBtn").click(function() {
 				if($('#about').hasClass('open')){
 					about.closeAbout();
